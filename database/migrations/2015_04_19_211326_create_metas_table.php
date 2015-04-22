4f419bb4b12aa207c9401b35834548bbb1e24592
Jefry <jefrycruz88@gmail.com>
2015-04-21 21:24:17 -0400

    Added more tables

--- database/migrations/2015_04_19_211326_create_metas_table.php
+++ database/migrations/2015_04_19_211326_create_metas_table.php
@@ -15,9 +15,15 @@ class CreateMetasTable extends Migration {
 		Schema::create('metas', function(Blueprint $table)
 		{
             $table->unsignedInteger('planet_id');
+            $table->string('name');
             $table->string('img');
             $table->integer('diameter');
+            $table->integer('radius');
             $table->string('type');
+            $table->integer('gravity');
+            $table->integer('distance_from_sun');
+            $table->integer('orbital_period');
+            $table->boolean('ring');
 			$table->timestamps();
 
             $table->foreign('planet_id')->references('id')->on('planets')->onDelete('cascade');
