8e071579fa5fbca227c70879e35f1b11f6079a22
Jefry <jefrycruz88@gmail.com>
2015-04-23 20:36:55 -0400

    Created Planet controller and basic route

---
+++ app/Http/Controllers/PlanetController.php
@@ -0,0 +1,21 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Planet;
+
+class PlanetController extends Controller {
+
+	// List all planets
+	public function index()
+	{
+		return Planet::all();
+	}
+
+	// Show a specific planet
+	public function show($id)
+	{
+		//
+	}
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,3 +19,8 @@ Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
+
+/**
+ * Planet related routes
+ */
+Route::get('/planets','PlanetController@index');
