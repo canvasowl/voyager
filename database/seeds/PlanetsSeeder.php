dde6012b6ab9beb91101508c3a0bfd76a65b9dcd
Jefry <jefrycruz88@gmail.com>
2015-04-25 15:17:31 -0400

    Create seed section for Mercury

--- database/seeds/PlanetsSeeder.php
+++ database/seeds/PlanetsSeeder.php
@@ -6,6 +6,21 @@ use App\Planet;
 class PlanetsSeeder extends Seeder {
     public function run()
     {
+
+        // Mercury
+        Planet::create(
+            [
+                'name' => 'Mercury',
+                'diameter' => 4878,
+                'radius' => 2440,
+                'type' => 'terrestrial',
+                'gravity' => 3.7,
+                'distance_from_sun' => 57910000,
+                'orbital_period' => 0.24,
+                'ring' => 0,
+            ]);
+
+        // Earth
         Planet::create(
             [
                 'name' => 'Earth',
