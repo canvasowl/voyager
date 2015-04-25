f96a3faea3062ac6f8a4b428521350756a54548f
Jefry <jefrycruz88@gmail.com>
2015-04-25 01:56:31 -0400

    Earth has been added

--- database/seeds/PlanetsSeeder.php
+++ database/seeds/PlanetsSeeder.php
@@ -6,6 +6,16 @@ use App\Planet;
 class PlanetsSeeder extends Seeder {
     public function run()
     {
-        Planet::create(['name' => 'Earth']);
+        Planet::create(
+            [
+                'name' => 'Earth',
+                'diameter' => 12756,
+                'radius' => 6371,
+                'type' => 'terrestrial',
+                'gravity' => 9.807,
+                'distance_from_sun' => 149500000,
+                'orbital_period' => 1,
+                'ring' => 0,
+            ]);
     }
 } 
