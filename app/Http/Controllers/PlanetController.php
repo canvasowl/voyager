edc42efac8e7a864f5ff3b480d25e567bef31e53
Jefry <jefrycruz88@gmail.com>
2015-05-09 14:38:35 -0400

    Added try catch block and response status code to api calls

--- app/Http/Controllers/PlanetController.php
+++ app/Http/Controllers/PlanetController.php
@@ -10,12 +10,30 @@ class PlanetController extends Controller {
 	// List all planets
 	public function index()
 	{
-		return Planet::all();
+        try{
+            $statusCode = 200;
+            $planets = Planet::all();
+            $response = ['statusCode' => $statusCode, 'planets' => $planets];
+        }catch (Exception $e){
+            $statusCode = 404;
+            $error = ["error" => "Could not find planets."];
+            $response = ['statusCode' => $statusCode, 'error' => $error];
+        }
+        return $response;
 	}
 
 	// Show a specific planet
-	public function show($id)
+	public function show($id = null)
 	{
-		return Planet::find($id);
+        try{
+            $statusCode = 200;
+            $planet = Planet::find($id);
+            $response = ['statusCode' => $statusCode, 'planet' => $planet];
+        }catch (Exception $e){
+            $statusCode = 404;
+            $error = ["error" => "Could not a planet."];
+            $response = ['statusCode' => $statusCode, 'error' => $error];
+        }
+        return $response;
 	}
 }
