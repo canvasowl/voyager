2518be42ada51fcfd6d54515fe2d0e92ce0ea607
Jefry <jefrycruz88@gmail.com>
2015-05-11 02:10:07 -0400

    Added mars and jupiter & based gravity relative to Earths gravity

--- database/seeds/PlanetsSeeder.php
+++ database/seeds/PlanetsSeeder.php
@@ -14,7 +14,7 @@ class PlanetsSeeder extends Seeder {
                 'diameter' => 4878,
                 'radius' => 6052,
                 'type' => 'terrestrial',
-                'gravity' => 8.87,
+                'gravity' => 0.38,
                 'distance_from_sun' => 108200000,
                 'orbital_period' => 0.62,
                 'ring' => 0,
@@ -27,7 +27,7 @@ class PlanetsSeeder extends Seeder {
                 'diameter' => 1204,
                 'radius' => 2440,
                 'type' => 'terrestrial',
-                'gravity' => 3.7,
+                'gravity' => 0.9,
                 'distance_from_sun' => 57910000,
                 'orbital_period' => 0.24,
                 'ring' => 0,
@@ -40,10 +40,36 @@ class PlanetsSeeder extends Seeder {
                 'diameter' => 12756,
                 'radius' => 6371,
                 'type' => 'terrestrial',
-                'gravity' => 9.807,
+                'gravity' => 1,
                 'distance_from_sun' => 149500000,
                 'orbital_period' => 1,
                 'ring' => 0,
             ]);
+
+        // Mars
+        Planet::create(
+            [
+                'name' => 'Mars',
+                'diameter' => 6787,
+                'radius' => 3390,
+                'type' => 'terrestrial',
+                'gravity' => 0.38,
+                'distance_from_sun' => 227900000,
+                'orbital_period' => 1.88,
+                'ring' => 0,
+            ]); 
+
+        // Jupiter
+        Planet::create(
+            [
+                'name' => 'Jupiter',
+                'diameter' => 11.209,
+                'radius' => 69911,
+                'type' => 'gas',
+                'gravity' => 2.64,
+                'distance_from_sun' => 778500000,
+                'orbital_period' => 11.86,
+                'ring' => yes,
+            ]);                    
     }
 } 
