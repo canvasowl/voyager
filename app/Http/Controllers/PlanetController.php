6f1fcf8400fcb423e92056790781c11f802090d8
Jefry <jefrycruz88@gmail.com>
2015-06-06 02:26:26 -0400

    Modified show method to handle retrieving planet property

--- app/Http/Controllers/PlanetController.php
+++ app/Http/Controllers/PlanetController.php
@@ -7,7 +7,7 @@ use App\Planet;
 
 class PlanetController extends Controller {
 
-	// List all planets
+	// List all planets.
 	public function index()
 	{
         try{
@@ -22,18 +22,41 @@ class PlanetController extends Controller {
         return $response;
 	}
 
-	// Show a specific planet
-	public function show($id = null)
+	// Show a specific planet or planet property.
+	public function show($id = null, $property = null)
 	{
-        try{
-            $statusCode = 200;
-            $planet = Planet::find($id);
-            $response = ['statusCode' => $statusCode, 'planet' => $planet];
-        }catch (Exception $e){
-            $statusCode = 404;
-            $error = ["error" => "Could not find a planet."];
-            $response = ['statusCode' => $statusCode, 'error' => $error];
+        $statusCode = 200;
+
+        // Find a single planet.
+        if($property == null){
+
+            try{
+                $planet = Planet::find($id);
+                $response = ['statusCode' => $statusCode, 'planet' => $planet];
+            }catch (Exception $e){
+                $statusCode = 404;
+                $error = ["error" => "Could not find a planet."];
+                $response = ['statusCode' => $statusCode, 'error' => $error];
+            }
+            return $response;
+
         }
-        return $response;
+        // Or find a specific property of a planet.
+        else {
+            try{
+                $val = Planet::find($id)->pluck($property);
+                $response = ['statusCode' => $statusCode, $property => $val];
+
+                return $response;
+
+            }catch (Exception $e){
+                $statusCode = 404;
+                $error = ["error" => "Could not property of planet."];
+                $response = ['statusCode' => $statusCode, 'error' => $error];
+
+                return $response;
+            }
+
+        }
 	}
 }
